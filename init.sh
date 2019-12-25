40f6c862555174ca0e1a14b50fbb0d418e567851
ericaltenburg <ealtenbu@stevens.edu>
2019-12-25 12:08:46 -0500

    Add init:

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS applications
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install spotify
+brew cask insatll skype
+
+# Install Fira Code Font
+brew tap homebrew/cask-fonts
+brew cask insatll font-fira-code
