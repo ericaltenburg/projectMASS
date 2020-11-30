a658ad88b4ed15550a3617fcaa51d65dbcf18f03
Eric Altenburg <ealtenbu@stevens.edu>
2020-11-30 18:36:42 -0500

    Upated the fs.sh file so it recursively goes through any file structure and looks for a .git folder before performing a git status.

--- fs.sh
+++ fs.sh
@@ -2,28 +2,47 @@
 
 # cd's into all repos, fetches and grabs status then echos out total number of outdated repos
 
-DIRECTORIES=*/
 WORK_DONE=false
+CHANGED_DIRECTORIES=()
 
-for directs in $DIRECTORIES
+function find_git {
+	DIRECTORIES=$1
+	for directs in $DIRECTORIES
+	do
+		cd $directs
+		if [ -d .git ]
+		then 
+			# echo Peeking into $directs
+			# echo Fetching Status...
+			git fetch -q > .to_be_removed.txt
+			rm .to_be_removed.txt
+			echo $(git status) > .temp.txt
+			if [ ! -z "$(grep "Untracked files" .temp.txt)" ] || [ ! -z "$(grep "Your branch is behind" .temp.txt)" ]
+			then 
+				CHANGED_DIRECTORIES+=( $directs )
+				WORK_DONE=true
+			fi
+			rm .temp.txt
+		else 
+			find_git "*/"
+		fi
+		cd ..
+	done
+}
+
+for i in */
 do
-	cd $directs 
-	git fetch
-	git status > .tempxyz.txt
-	if [[ -z $(grep nothing .tempxyz.txt) ]]; then
-		echo "$directs"
-		WORK_DONE=true
-	fi
-
-	if [[ ! -z $(grep behind .tempxyz.txt) ]]; then
-		echo "$directs"
-		WORK_DONE=true
-	fi
-	rm .tempxyz.txt
-	cd - > /dev/null
+	find_git "$i"
 done
 
-if [ "$WORK_DONE" == false ]; then
+if [ "$WORK_DONE" == false ]
+then
 	echo "All repositories are up-to-date"
+else 
+	echo "Repositories out-of-date:"
+	for d in ${CHANGED_DIRECTORIES[@]}
+	do
+		echo $d
+	done
 fi
 
